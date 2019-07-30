3eabdb7dc10910d8c6313fd6a51b5860f7853e4d
tuliobarreto <tuliopaesbarreto@gmail.com>
2019-07-29 21:53:20 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MeuPrimeiroComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
